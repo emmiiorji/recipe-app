88a9649e212b4dbc403684c2c9b4740f42f41350
kiboma <omayioben@gmail.com>
2022-11-09 00:12:49 +0300

    Create food controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'food/index'
   devise_for :users
+  root 'food#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
