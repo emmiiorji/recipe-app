5cd0a7babb30ef3177d74d813706865c4d99b406
kiboma <omayioben@gmail.com>
2022-11-10 00:36:36 +0300

    Add association betweeen food and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_163522) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_213544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_163522) do
     t.integer "quantinty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
