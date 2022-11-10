22bf17e40b4acf304ca7a9ff8342cd6a8ee9071a
kiboma <omayioben@gmail.com>
2022-11-10 23:59:43 +0300

    Add controller and model for recipe food

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_200701) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_205759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_200701) do
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.integer "quantity", null: false
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "recipe_id"
+    t.integer "food_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
