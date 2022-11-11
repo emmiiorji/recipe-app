61c9c09ed3bb0586b8e24fc54c2b86c691dcec6b
kiboma <omayioben@gmail.com>
2022-11-12 01:15:19 +0300

    Update recipe details and shoping list

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -23,21 +23,10 @@ class RecipeFoodsController < ApplicationController
   # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
-  #  recipe = Recipe.find(params[:recipe_id])
-  #  @recipe_food = RecipeFood.new(params.require(:recipe_food).permit(:quantity, :recipe, :food_id))
-  #  @recipe_food.recipe = recipe
-
-    respond_to do |format|
-      if @recipe_food.save
-        #redirect_to recipe_foods_path
-        format.html do
-          redirect_to user_recipe_path(current_user, recipe), notice: 'Recipe food was successfully created.'
-        end
-        format.json { render :show, status: :created, location: @recipe_food }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
-      end
+    if @recipe_food.save
+      redirect_to recipe_foods_path
+    else
+      render 'new'
     end
   end
 
